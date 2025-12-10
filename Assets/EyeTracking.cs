d4bf67b6f2515b7b7cccb1a9c9b6243786219704
sonia-puertolasm <sonia.puertolas.martinez@gmail.com>
2025-12-10 11:23:44 +0100

    Updates 10.12 v3

--- Assets/EyeTracking.cs
+++ Assets/EyeTracking.cs
@@ -10,7 +10,9 @@ public class EyeTracking : MonoBehaviour
     private FocusSystem focusSystem;
     private GameObject centerFixationPoint;
     private GameObject gridLinesParent;
+    private GameObject gridPointsParent;
     private GameObject probesParent;
+    private Transform centerOriginalParent;
 
     private bool hideAllExceptCenter = false;
 
@@ -38,12 +40,19 @@ public class EyeTracking : MonoBehaviour
             if (centerTransform != null)
             {
                 centerFixationPoint = centerTransform.gameObject;
+                centerOriginalParent = centerTransform.parent;
             }
 
             Transform gridLinesTransform = mainGrid.transform.Find("GridLines");
             if (gridLinesTransform != null)
             {
                 gridLinesParent = gridLinesTransform.gameObject;
+            }
+
+            Transform gridPointsTransform = mainGrid.transform.Find("GridPoints");
+            if (gridPointsTransform != null)
+            {
+                gridPointsParent = gridPointsTransform.gameObject;
             }
         }
 
@@ -94,43 +103,47 @@ public class EyeTracking : MonoBehaviour
             focusSystem.ExitFocusMode();
         }
 
-        // Disable grid lines parent
+        if (centerFixationPoint != null && centerOriginalParent != null)
+        {
+            centerFixationPoint.transform.SetParent(null);
+        }
+
         if (gridLinesParent != null)
         {
             gridLinesParent.SetActive(false);
         }
 
-        // Disable probes parent (all probes will be hidden)
+        if (gridPointsParent != null)
+        {
+            gridPointsParent.SetActive(false);
+        }
+
         if (probesParent != null)
         {
             probesParent.SetActive(false);
-        }
-
-        // Ensure center fixation point remains visible
-        if (centerFixationPoint != null)
-        {
-            centerFixationPoint.SetActive(true);
         }
     }
 
     private void ShowAllElements()
     {
-        // Enable grid lines parent
+        if (gridPointsParent != null)
+        {
+            gridPointsParent.SetActive(true);
+        }
+
+        if (centerFixationPoint != null && centerOriginalParent != null)
+        {
+            centerFixationPoint.transform.SetParent(centerOriginalParent);
+        }
+
         if (gridLinesParent != null)
         {
             gridLinesParent.SetActive(true);
         }
 
-        // Enable probes parent (all probes will be shown)
         if (probesParent != null)
         {
             probesParent.SetActive(true);
-        }
-
-        // Ensure center fixation point remains visible
-        if (centerFixationPoint != null)
-        {
-            centerFixationPoint.SetActive(true);
         }
 
         if (gridRebuildManager != null)
